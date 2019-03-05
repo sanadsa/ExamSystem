676d98b5ff7491b97d11d18546b9ca8f641e3381
omercoen050 <omercoen050@gmail.com>
2019-03-05 18:53:51 +0200

    a

--- Exam-Client/src/app/components/exam-result/exam-result.component.ts
+++ Exam-Client/src/app/components/exam-result/exam-result.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Test } from 'src/app/models/test';
-import { Question, eQuestionType } from 'src/app/models/question';
-import { ExamService } from 'src/app/services/exam.service';
+import { Question } from 'src/app/models/question';
+import { TestResult } from 'src/app/models/test-result';
 
 @Component({
   selector: 'app-exam-result',
@@ -10,74 +10,83 @@ import { ExamService } from 'src/app/services/exam.service';
   styleUrls: ['./exam-result.component.css']
 })
 export class ExamResultComponent implements OnInit {
-private questionType = eQuestionType;
-
   test: Test;
+  testReuslts: TestResult[] = []
   questions: Question[]
   allAnswers: any[]
   answers: any[]
-  userId: number
-  userAnswers: any
-  q: Question
+  question: Question
   index: number = 0;
-  numberOfRightAnswers: number = 0;
-  grade:number;
-  status:string;
-  constructor(private route: ActivatedRoute, private examService: ExamService) { }
+  numberOfRightAnswers: number;
+  grade: number;
+  status: string;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.test = JSON.parse(params.get('test'));
       this.questions = JSON.parse(params.get('questions'));
       this.allAnswers = JSON.parse(params.get('answers'));
-      this.q = this.questions[this.index];
-      this.answers = this.allAnswers.filter(a => a.QuestionId == this.q.ID);
-      this.userId = parseInt(params.get('userId'));
-      for (let index = 0; index < this.questions.length; index++) {
-      this.answers = this.allAnswers.filter(a => a.QuestionId == this.questions[index].ID);
-      this.answers.forEach(answer => {
-        if (this.questions[index].QuestionType == eQuestionType.SingleChoice) {
-          if (answer.Selected && answer.CorrectAnswer) {
-            this.numberOfRightAnswers++;
-          }
-        }
-        else{
-          
-        }
-      });
-      }
-      this.calculateGrade();
+      this.question = this.questions[this.index];
+      this.answers = this.allAnswers.filter(a => a.QuestionId == this.question.ID);
+      this.checkTestReult();
     })
   }
 
+  checkTestReult() {
+    for (let index = 0; index < this.questions.length; index++) {
+      const answers = this.allAnswers.filter(a => a.QuestionId == this.questions[index].ID);
+      debugger;
+      const correctAnswersId = answers.filter(a => a.CorrectAnswer).map(a => a.ID);
+      const selectedAnswersId = answers.filter(a => a.Selected).map(a => a.ID);
+      const testResult = {
+        questionId: this.questions[index].ID,
+        correctAnswersId: correctAnswersId,
+        selectedAnswersId: selectedAnswersId,
+        userAnsweredCorrectly: true
+      }
+      this.testReuslts.push(testResult);
+    }
+    for (let i = 0; i < this.testReuslts.length; i++) {
+      for (let j = 0; j < this.testReuslts[i].correctAnswersId.length; j++) {
+        if (this.testReuslts[i].correctAnswersId[j] != this.testReuslts[i].selectedAnswersId[j]) {
+          this.testReuslts[i].userAnsweredCorrectly = false;
+        }
+      }
+    }
+    this.numberOfRightAnswers = this.testReuslts.filter(tr => tr.userAnsweredCorrectly == true).length;
+    this.calculateGrade();
+  }
+
   calculateGrade() {
-    debugger;
     this.grade = 100 / this.questions.length * this.numberOfRightAnswers;
     if (this.grade > this.test.PassingGrade) {
       this.status = 'Passed';
-    }else{
+    } else {
       this.status = 'Failed';
     }
   }
 
   previousQuestion() {
     this.index--;
-    this.q = this.questions[this.index];
-    if (!this.q) {
+    this.question = this.questions[this.index];
+    if (!this.question) {
       this.index++;
-    this.q = this.questions[this.index];
+      this.question = this.questions[this.index];
     }
-    this.answers = this.allAnswers.filter(a => a.QuestionId == this.q.ID);
+    this.answers = this.allAnswers.filter(a => a.QuestionId == this.question.ID);
   }
 
   nextQuestion() {
     this.index++;
-    this.q = this.questions[this.index];
-    if (!this.q) {
+    this.question = this.questions[this.index];
+    if (!this.question) {
       this.index--;
-    this.q = this.questions[this.index];
+      this.question = this.questions[this.index];
     }
-    this.answers = this.allAnswers.filter(a => a.QuestionId == this.q.ID);
+    this.answers = this.allAnswers.filter(a => a.QuestionId == this.question.ID);
   }
 
 }
+
+
